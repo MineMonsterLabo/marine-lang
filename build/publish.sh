db6b7023cf7e7e0601fc7aec1ec30a214080bc4b
hiroki199990625 <hiroki19990625@gmail.com>
2020-10-01 03:30:43 +0900

    [integration] nugetパッケージへのパブリッシュ
    コミットハッシュを追加

--- build/publish.sh
+++ build/publish.sh
@@ -9,7 +9,9 @@ if [ ${?} = "128" ]; then
   exit 0
 fi
 
-# dotnet pack MarineLang/MarineLang.csproj -c Release --include-source --include-symbols -o build/artifacts -p:PackageVersion=$TAG_VERSION
-dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:PackageVersion=$TAG_VERSION
+COMMIT_HASH=`git rev-parse --short HEAD`
+
+# dotnet pack MarineLang/MarineLang.csproj -c Release --include-source --include-symbols -o build/artifacts -p:PackageVersion="${TAG_VERSION}+${COMMIT_HASH}"
+dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:PackageVersion="${TAG_VERSION}+${COMMIT_HASH}"
 
 dotnet nuget push "build/artifacts/MarineLang.${TAG_VERSION}.nupkg" -s https://api.nuget.org/v3/index.json -k $API_KEY
