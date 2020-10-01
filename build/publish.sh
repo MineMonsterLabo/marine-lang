3d2b520d00b465d5039f5101b6f0f06f0431848a
hiroki <hiroki19990625@gmail.com>
2020-10-02 00:36:06 +0800

    Update publish.sh

--- build/publish.sh
+++ build/publish.sh
@@ -9,9 +9,15 @@ if [ ${?} = "128" ]; then
   exit 0
 fi
 
+echo "release version ${TAG_VERSION}"
+
 COMMIT_HASH=`git rev-parse --short HEAD`
 
 # dotnet pack MarineLang/MarineLang.csproj -c Release --include-source --include-symbols -o build/artifacts -p:PackageVersion="${TAG_VERSION}+${COMMIT_HASH}"
 dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:PackageVersion="${TAG_VERSION}+${COMMIT_HASH}"
 
+echo "package created"
+
 dotnet nuget push "build/artifacts/MarineLang.${TAG_VERSION}.nupkg" -s https://api.nuget.org/v3/index.json -k $API_KEY
+
+echo "pushed"
