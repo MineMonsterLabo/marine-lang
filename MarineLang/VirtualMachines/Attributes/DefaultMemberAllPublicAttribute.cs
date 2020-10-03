e9505b86cab1811676d12b3af5d83a8b1009acff
hiroki199990625 <hiroki19990625@gmail.com>
2020-10-03 20:19:11 +0900

    [delete] 不要な属性を削除

--- MarineLang/VirtualMachines/Attributes/DefaultMemberAllPublicAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MarineLang.VirtualMachines.Attributes
-{
-    [AttributeUsage(AttributeTargetConstants.ClassTarget, Inherited = false)]
-    public sealed class DefaultMemberAllPublicAttribute : Attribute
-    {
-    }
-}
