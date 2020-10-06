20f58ef8493f7319176d02fe712960a062860bda
hiroki19990625 <hiroki19990625@gmail.com>
2020-10-07 08:47:16 +0900

    integration コミットハッシュではなく、コミット数でバージョンをナンバリング

--- build/publish.sh
+++ build/publish.sh
@@ -12,15 +12,17 @@ TAG_VERSION="0.1.0-alpha"
 
 echo "release version ${TAG_VERSION}"
 
-COMMIT_HASH=`git rev-parse --short HEAD`
+# COMMIT_HASH=`git rev-parse --short HEAD`
+COMMIT_COUNT=`git log --oneline | wc -l`
 
-echo "release version hash ${COMMIT_HASH}"
+# echo "release version hash ${COMMIT_HASH}"
+echo "release version hash ${COMMIT_COUNT}"
 
-# dotnet pack MarineLang/MarineLang.csproj -c Release -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_HASH}"
-dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_HASH}"
+# dotnet pack MarineLang/MarineLang.csproj -c Release -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_COUNT}"
+dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_COUNT}"
 
 echo "package created"
 
-dotnet nuget push "build/artifacts/MarineLang.${TAG_VERSION}.${COMMIT_HASH}.nupkg" --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
+dotnet nuget push "build/artifacts/MarineLang.${TAG_VERSION}.${COMMIT_COUNT}.nupkg" --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
 
 echo "pushed"
