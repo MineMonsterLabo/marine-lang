8e012975f18d62b56b25e9e730c26fdd769f9a04
elipmoc <relipmoc101@gmail.com>
2020-10-08 00:59:40 +0900

    [feat] ErrorCodeとエラーメッセージの対応関係を作成

--- MarineLang/Models/Errors/ErrorCode.cs
+++ MarineLang/Models/Errors/ErrorCode.cs
@@ -1,4 +1,6 @@
-﻿namespace MarineLang.Models.Errors
+﻿using System;
+
+namespace MarineLang.Models.Errors
 {
     public enum ErrorCode
     {
@@ -11,5 +13,34 @@
         NonLetEqual,
         NonEqualExpr,
         Unknown
+    }
+
+    public static class ErrorCodeExtension
+    {
+        public static string ToErrorMessage(this ErrorCode errorCode)
+        {
+            switch (errorCode)
+            {
+                case ErrorCode.NonEndWord:
+                    return "関数の終わりにendがありません";
+                case ErrorCode.NonFuncWord:
+                    return "関数定義が間違っています";
+                case ErrorCode.NonFuncName:
+                    return "関数定義に関数名がありません";
+                case ErrorCode.NonFuncParen:
+                    return "関数定義には()が必要です";
+                case ErrorCode.NonRetExpr:
+                    return "retの後には式が必要です";
+                case ErrorCode.NonLetVarName:
+                    return "letの後には変数名が必要です";
+                case ErrorCode.NonLetEqual:
+                    return "letに=がありません";
+                case ErrorCode.NonEqualExpr:
+                    return "=の後に式がありません";
+                case ErrorCode.Unknown:
+                    return "";
+            }
+            throw new NotImplementedException();
+        }
     }
 }
