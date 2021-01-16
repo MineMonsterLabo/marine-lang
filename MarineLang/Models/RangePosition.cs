2cb35c306a51593927e546c0c7a3bed2bee262b8
relipmoc101 <relipmoc101@gmail.com>
2021-01-16 17:10:23 +0900

    [feat] RangePositionに便利なメソッドを追加

--- MarineLang/Models/RangePosition.cs
+++ MarineLang/Models/RangePosition.cs
@@ -2,12 +2,12 @@
 
 namespace MarineLang.Models
 {
-   public class RangePosition:IEquatable<RangePosition>
+    public class RangePosition : IEquatable<RangePosition>
     {
-        public Position Start{ get; }
-        public Position End{ get; }
+        public Position Start { get; }
+        public Position End { get; }
 
-        public RangePosition(Position start,Position end)
+        public RangePosition(Position start, Position end)
         {
             Start = start;
             End = end;
@@ -20,7 +20,36 @@ namespace MarineLang.Models
 
         public bool Equals(RangePosition other)
         {
-            return Start.Equals( other.Start )&& End.Equals(other.End);
+            return Start.Equals(other.Start) && End.Equals(other.End);
+        }
+
+        public bool Contain(Position position)
+        {
+            return
+                Start.line < position.line &&
+                End.line > position.line
+                ||
+                Start.line == position.line &&
+                End.line == position.line &&
+                Start.column <= position.column &&
+                End.column >= position.column
+                ||
+                Start.line == position.line &&
+                Start.line != End.line &&
+                Start.column <= position.column
+                ||
+                End.line == position.line &&
+                Start.line != End.line &&
+                End.column >= position.column;
+        }
+
+        public bool Intersection(RangePosition rangePosition)
+        {
+            return
+                Contain(rangePosition.Start) ||
+                Contain(rangePosition.End) ||
+                rangePosition.Contain(Start) ||
+                rangePosition.Contain(End);
         }
     }
 }
