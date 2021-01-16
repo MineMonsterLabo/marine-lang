6ab110cef1ae6037a2ede429a627497fb5f7d43e
relipmoc101 <relipmoc101@gmail.com>
2021-01-16 17:10:37 +0900

    [test] RangePositionのテスト作成

---
+++ MarineLangUnitTest/RangePositionTest.cs
@@ -0,0 +1,56 @@
+﻿using MarineLang.Models;
+using Xunit;
+
+namespace MarineLangUnitTest
+{
+    public class RangePositionTest
+    {
+        [Theory]
+        [InlineData(1, 1, 1, 1, 1, 1, true)]
+        [InlineData(1, 1, 1, 10, 1, 8, true)]
+        [InlineData(1, 1, 1, 10, 1, 10, true)]
+        [InlineData(1, 1, 5, 1, 1, 10, true)]
+        [InlineData(1, 1, 5, 1, 2, 10, true)]
+        [InlineData(1, 1, 5, 1, 5, 1, true)]
+        [InlineData(1, 1, 5, 1, 5, 2, false)]
+        [InlineData(1, 1, 5, 1, 0, 10, false)]
+        [InlineData(1, 1, 1, 1, 1, 2, false)]
+        public void ContainTest(
+            int startLine, int startColumn,
+            int endLine, int endColumn,
+            int line, int column,
+            bool isContain
+        )
+        {
+            var rangePosition
+                = new RangePosition(new Position(startLine, startColumn), new Position(endLine, endColumn));
+            var position = new Position(line, column);
+            Assert.Equal(isContain, rangePosition.Contain(position));
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, 1, 1, 1, 1, true)]
+        [InlineData(1, 1, 1, 10, 1, 10, 1, 10, true)]
+        [InlineData(1, 5, 1, 10, 1, 1, 1, 5, true)]
+        [InlineData(1, 5, 1, 10, 1, 6, 1, 8, true)]
+        [InlineData(1, 5, 1, 6, 1, 7, 1, 8, false)]
+        [InlineData(1, 7, 1, 8, 1, 5, 1, 6, false)]
+        [InlineData(1, 5, 2, 10, 1, 15, 1, 16, true)]
+        [InlineData(1, 5, 2, 10, 2, 10, 2, 10, true)]
+        [InlineData(1, 5, 2, 10, 2, 11, 3, 4, false)]
+        public void IntersectionTest(
+           int startLine, int startColumn,
+           int endLine, int endColumn,
+           int start2Line, int start2Column,
+           int end2Line, int end2Column,
+           bool isContain
+       )
+        {
+            var rangePosition
+                = new RangePosition(new Position(startLine, startColumn), new Position(endLine, endColumn));
+            var rangePosition2
+                = new RangePosition(new Position(start2Line, start2Column), new Position(end2Line, end2Column));
+            Assert.Equal(isContain, rangePosition.Intersection(rangePosition2));
+        }
+    }
+}
