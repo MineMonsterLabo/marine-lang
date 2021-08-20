c053606605469845fadba96bd446f26d93614d7c
hiroki19990625 <hiroki19990625@gmail.com>
2021-08-21 03:58:59 +0900

    シェル芸でファイル名を見つけるように

--- build/publish.sh
+++ build/publish.sh
@@ -2,28 +2,14 @@
 
 echo "start publish."
 
-# TAG_VERSION=`git describe --exact-match`
-# TAG_VERSION="0.1.0-alpha"
-
-# if [ ${?} = "128" ]; then
-#   echo "tag not found."
-#   exit 0
-# fi
-
-# echo "release version ${TAG_VERSION}"
-
-# COMMIT_HASH=`git rev-parse --short HEAD`
-COMMIT_COUNT=`git log --oneline | wc -l`
-
-# echo "release version hash ${COMMIT_HASH}"
-echo "release version hash ${COMMIT_COUNT}"
-
 # dotnet pack MarineLang/MarineLang.csproj -c Release -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_COUNT}"
-# dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_COUNT}"
-nuget pack MarineLang/MarineLang.csproj -Build -IncludeReferencedProjects -Symbols -SymbolPackageFormat snupkg -OutputFileNamesWithoutVersion
+dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 echo "package created"
 
-dotnet nuget push "MarineLang.nupkg" --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
+FILE_NAME=`find build/artifacts -name "*.nupkg"`
+echo "find nupkg ${FILE_NAME}"
+
+dotnet nuget push ${FILE_NAME} --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
 
 echo "pushed"
