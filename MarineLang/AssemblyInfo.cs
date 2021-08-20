ad5292fe3ec1050990a307379eb45af29fcd79ef
hiroki19990625 <hiroki19990625@gmail.com>
2021-08-20 20:48:35 +0900

    バージョニング管理変更準備

---
+++ MarineLang/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyFileVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.Commits + "+" +
+    ThisAssembly.Git.Commit)]
