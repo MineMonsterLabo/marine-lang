672a1496242402c6b8debdb8b8be853d1b4c19ba
hiroki19990625 <hiroki19990625@gmail.com>
2022-06-15 01:37:59 +0900

    feat ビルドスクリプトに追記

--- build/publish.sh
+++ build/publish.sh
@@ -6,12 +6,18 @@ mkdir build/artifacts
 
 # dotnet pack MarineLang/MarineLang.csproj -c Release -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -p:PackageVersion="${TAG_VERSION}.${COMMIT_COUNT}"
 dotnet pack MarineLang/MarineLang.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack MarineLang.CodeAnalysis/MarineLang.CodeAnalysis.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack MarineLang.CodeDom/MarineLang.CodeDom.csproj --include-source --include-symbols -o build/artifacts -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 echo "package created"
 
-FILE_NAME=`find build/artifacts -name "*.nupkg"`
-echo "find nupkg ${FILE_NAME}"
+FILE_NAMES=`find build/artifacts -name "*.nupkg"`
+echo "find nupkg files"
+echo "${FILE_NAMES}"
 
-dotnet nuget push ${FILE_NAME} --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
+for file in ${FILE_NAMES}; do
+	echo "processing ${file}"
+	dotnet nuget push ${file} --api-key "${API_KEY}" --source "https://api.nuget.org/v3/index.json"
+done
 
 echo "pushed"
