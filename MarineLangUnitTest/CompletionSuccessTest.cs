886110c3b29bbfb5741e997b6076ac7354cefcb4
hiroki19990625 <hiroki19990625@gmail.com>
2022-06-18 17:30:39 +0900

    fix テスト修正

--- MarineLangUnitTest/CompletionSuccessTest.cs
+++ MarineLangUnitTest/CompletionSuccessTest.cs
@@ -12,12 +12,37 @@ namespace MarineLangUnitTest
         public void Test()
         {
             var nl = Environment.NewLine;
-            var source = $"fun main(i){nl}if i > 0 {{ let a = 5 }}{nl}end";
+            var source = $"fun main(i){nl}if i > 0★ {{ let a = 5 }}{nl}end";
 
+            var position = GetMarkerPosition(source);
+            Assert.NotNull(position);
+
+            source = source.Replace("★", string.Empty);
+
+            const int keywordCount = 6;
             MarineLangWorkspace workspace = new MarineLangWorkspace(string.Empty);
             workspace.SetTextDocument("test.mrn", source);
             var context = workspace.GetCompletionContext("test.mrn");
-            var list = context.GetCompletions(new Position(2, 9)).ToArray();
+            var list = context.GetCompletions(position.Value).ToArray();
+            Assert.NotNull(list);
+            Assert.Equal(keywordCount + 2, list.Length);
+        }
+
+        private Position? GetMarkerPosition(string source, string marker = "★")
+        {
+            Position? position = null;
+            var lines = source.Split(Environment.NewLine);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var index = line.IndexOf(marker, StringComparison.Ordinal);
+                if (index != -1)
+                {
+                    position = new Position(i + 1, index + 1);
+                }
+            }
+
+            return position;
         }
     }
 }
